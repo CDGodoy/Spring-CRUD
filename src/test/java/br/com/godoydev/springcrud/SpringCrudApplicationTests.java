35de843afc9b3e2cadcc613c0988d1c71f3c8200
Carlos <cdgodoy17@gmail.com>
2022-05-08 15:58:23 +0100

    First commit

---
+++ src/test/java/br/com/godoydev/springcrud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.godoydev.springcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
